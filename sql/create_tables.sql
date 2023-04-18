d7cbff2ee7030129c9de481d8ecd62ab49073d32
Lorenzo Hidalgo <lorenzohidalgogadea@gmail.com>
2023-04-18 10:37:27 +0200

    removed s3Path

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS AnimalPictures (
     pictureId INT NOT NULL AUTO_INCREMENT,
     animalType VARCHAR(250) NOT NULL,
-    s3Path VARCHAR(250) NOT NULL,
     s3ARN VARCHAR(250) NOT NULL,
     RecordCreatedAt datetime NOT NULL DEFAULT current_timestamp,
     RecordModifiedAt datetime NOT NULL DEFAULT current_timestamp,
